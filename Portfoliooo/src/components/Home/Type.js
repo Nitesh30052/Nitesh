b416a14e40013e59b9d5f96ad0e9ce4e84e890b1
Nitesh30052 <147510362+Nitesh30052@users.noreply.github.com>
2024-06-22 16:09:31 +0530

    comit

---
+++ Portfoliooo/src/components/Home/Type.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Student at KL University",
+          "Getting specalized in Data Science",
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
